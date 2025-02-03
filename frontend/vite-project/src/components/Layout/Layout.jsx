040038887b9a757aa17c83feb1763ccbfc1645d4
Dhanunjaya Ginjupalli <dhanunjaya.lakshmi60@gmail.com>
2025-02-04 00:53:26 +0530

    Created Hero, Header, Logo and Layout files

---
+++ frontend/vite-project/src/components/Layout/Layout.jsx
@@ -0,0 +1,12 @@
+import { Header } from "./Header";
+import { Hero } from "./Hero";
+
+export const Layout = (children) => {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Header />
+      <Hero />
+      <main className="container mx-auto px-4 py-12 -mt-8">{children}</main>
+    </div>
+  );
+};
